25ab7377de37037064b03ecc03fd36db45c3154b
f.monteiro <fernando@newaeonweb.com.br>
2014-11-20 10:56:54 -0200

    added script to version download

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
-$(document).ready(function() {
-    $('.img-icon').viewportChecker({
+(function($){
+    // Image effect
+	$('.img-icon').viewportChecker({
         classToAdd: 'visible animated rotateIn',
 		offset: 100,
 		repeat: false
     });
-});
+	
+	
+	// get download link and version from github repo
+    $.get("package.json", {nocache: Math.random()}, function(data){
+
+        $(function(){
+            $("[data-krack-download]").attr("href", "https://github.com/newaeonweb/frontendboilerplate/releases/download/v"+data.version+"/frontendboilerplate-"+data.version+".zip")
+            $("[data-krack-version]").text("Version "+data.version);
+        });
+
+    }, 'json');
+	
+	// get stars and forks from github
+	$.ajax({
+        dataType : "jsonp",
+        url      : "https://api.github.com/repos/newaeonweb/frontendboilerplate?callback=ukghapi&nocache="+Math.random(),
+        success  : function(data){
+            if(!data) return;
+                    if(data.data.watchers){
+                        $("[data-krack-stargazers]").html(data.data.watchers);
+                    }
+                    if(data.data.forks){
+                        $("[data-krack-forks]").html(data.data.forks);
+                    }
+                }
+            });
+})(jQuery);
--- js/vendor/viewPortChecker.js
+++ js/vendor/viewPortChecker.js
@@ -14,7 +14,6 @@
     The above copyright notice and this permission notice shall be included in all
     copies or substantial portions of the Software.
 */
-
 (function($){
     $.fn.viewportChecker = function(useroptions){
         // Define options and extend with user
